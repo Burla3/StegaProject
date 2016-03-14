059966b52ad5e96c3f14946e6e55aa96d7aca5c8
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-03-14 13:02:53 +0100

    Added EntropyComponent and Decoder

---
+++ StegaProject/StegaProject/Decoder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HuffmanTreeBuilder;
+
+namespace StegaProject {
+    class Decoder {
+
+        private List<HuffmanTree> buildHuffmanTrees;
+
+        private List<string> huffmanTrees;
+
+        private string binaryData;
+
+
+        public Decoder(string path) {
+
+            JPEGExtractor s = new JPEGExtractor();
+            s.LoadImage(path);
+
+            string data = s.GetCompressedImageData();
+
+            huffmanTrees = s.GetDHT();
+
+            foreach (string huffmanTree in huffmanTrees) {
+                Console.WriteLine(huffmanTree);
+            }
+
+            buildHuffmanTrees = new List<HuffmanTree>();
+
+            foreach (string tree in huffmanTrees) {
+                buildHuffmanTrees.Add(new HuffmanTree(tree));
+            }
+
+            for (int i = 0; i < data.Length; i++) {
+                if (data[i] != ' ') {
+                    binaryData += Convert.ToString(Convert.ToInt32(data[i].ToString(), 16), 2).PadLeft(4, '0');
+                }
+            }
+
+            Console.WriteLine(data);
+            Console.WriteLine(binaryData);
+
+            //int currentIndex = 0;
+            //string currentHuffmanCode = "";
+            //string huffmanLeaveValue = "";
+
+            //while (currentIndex <= binaryData.Length) {
+
+            //    for
+
+            //    while (huffmanLeaveValue == "") {
+            //            currentHuffmanCode += binaryData[currentIndex];
+            //            huffmanLeaveValue = buildHuffmanTrees[0].SearchFor(currentHuffmanCode);
+            //        }
+
+
+            //}
+        }
+    }
+}
--- StegaProject/StegaProject/Form1.cs
+++ StegaProject/StegaProject/Form1.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using HuffmanTreeBuilder;
 
 namespace StegaProject {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            Decoder decoder = new Decoder(@"C:\Users\Mikke\Desktop\small-rainbow-60-web-optimized.jpg");
+
         }
     }
 }
